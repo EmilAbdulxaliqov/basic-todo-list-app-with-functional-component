6ebb7ff2ea5b02014738209d48face52c6ba13a2
Emil Abdulxalıqov <emilabdulxaliqov@Emils-MacBook-Air.local>
2022-02-12 20:45:19 +0400

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {ListItem, ListItemText, Container, Button, TextField, Stack} from '@mui/material/'
 
 function App() {
+  const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState([])
+  const updateItem = (item) => {
+    setNewItem(item)
+  }
+  const addItem = () => {
+    const list = items
+    const item = {
+      id: new Date().getTime(),
+      value: newItem
+    }
+    list.push(item)
+    setItems(list)
+    setNewItem('')
+  }
+  const deleteItem = (id) => {
+    const list = items
+    const newList = list.filter(item => item.id !== id)
+    console.log(list)
+    setItems(newList)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Stack direction="column" alignItems="center" justifyContent="center" spacing={2}>
+          <h1>TODO List</h1>
+          <ListItem sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center'}}>
+            {items.map(item => (
+              <React.Fragment key={item.id}>
+                <ListItemText>{item.value}</ListItemText>
+                <Button onClick={() => deleteItem(item.id)} variant="contained">X</Button>
+              </React.Fragment>
+            ))}
+          </ListItem>
+          <TextField onChange={e => updateItem(e.target.value)}sx={{width: 400}}  label="Add Item"/>
+          <Button onClick={() => addItem()} sx={{width: 400}} variant="contained">Add item</Button>
+        </Stack>
+      </Container>
+      
+      
     </div>
   );
 }
-
+// style={{width: '300px'}}
 export default App;
